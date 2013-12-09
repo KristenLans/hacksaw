c5977dc919631aedee62a629f8934b937de47ef0
Anthony Goddard <anthony@anthonygoddard.com>
2013-12-09 09:54:43 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hacksaw::Application.config.secret_key_base = 'e27005dd375de7394a7b4abee88c60379956e82d8b5427f809da8b6b5d34d52f7eceab2b7b9cb70d9082ea7d53bad8d75a3bf71986a600f2b5c1ed614f735b05'
