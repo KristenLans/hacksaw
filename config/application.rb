2fcdad483089d1ac5eea96fbc5b74e56994281e1
Lisa Walley <lwalley@eol.org>
2013-12-09 11:21:02 -0500

    Added d3 javascript library.

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,6 @@ module Hacksaw
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.precompile += %w( d3 )
   end
 end
